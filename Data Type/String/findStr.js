e4766e8e3e4621e0f2f11a51eebed3a09a195c81
Cheikh GAYE <gayec270198@gmail.com>
2023-09-21 22:49:53 +0000

    String

---
+++ Data Type/String/findStr.js
@@ -0,0 +1,5 @@
+'use strict';
+function checkSpam(str) {
+    let lowerStr = str.toLowerCase();
+    return lowerStr.includes('viagra') || lowerStr.includes('xxx');
+}
---
+++ Data Type/String/upper.js
@@ -0,0 +1,7 @@
+'use strict'
+function ucFirst(str) {
+    if (!str) {
+      return str;
+    }
+    return str.at(0).toUpperCase() + str.slice(1);
+}
